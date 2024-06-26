c950b8ee4894e810b4314e2a5075ede874bf96b5
WangXueFei11 <144666483+WangXueFei11@users.noreply.github.com>
2024-06-26 11:08:32 +0800

    Create __init__.py

---
+++ Project_2/rsnet-Cifar-10/models/__init__.py
@@ -0,0 +1,14 @@
+from .AlexNet import *
+from .VGG import *
+from .GoogLeNet import *
+from .ResNet import *
+from .DenseNet import *
+
+from .SENet import *
+from .CBAM import *
+from .ECANet import *
+
+from .SqueezeNet import *
+from .MobileNet import *
+from .ShuffleNet import *
+from .Xception import *
